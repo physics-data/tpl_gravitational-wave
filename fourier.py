75d47d2a44e0810a17457997678c697828dff5a3
Benda Xu <heroxbd@gentoo.org>
2019-07-19 16:21:31 +0800

    fourier.py: 画出原始数据的频谱。

---
+++ fourier.py
@@ -0,0 +1,26 @@
+from plot_raw import load
+import sys, numpy as np
+
+strain_H1, strain_L1, time, event = load(sys.argv[2], "GW150914")
+
+fs = event['fs'] # 4096Hz by default
+N = len(strain_H1)
+dt = 1.0 / fs
+
+kH1 = np.abs(np.fft.rfft(strain_H1)) * 2/N
+kL1 = np.abs(np.fft.rfft(strain_L1)) * 2/N
+freq = np.fft.rfftfreq(N, dt)
+
+import matplotlib as mpl
+mpl.use("Agg")
+from matplotlib import pyplot as plt
+
+plt.figure(figsize = (10,7))
+plt.loglog(freq, kH1, label="H1")
+plt.loglog(freq, kL1, label="L1")
+plt.legend()
+
+plt.xlim(20, 2000)
+plt.xlabel('frequency [Hz]')
+plt.grid()
+plt.savefig(sys.argv[1])
