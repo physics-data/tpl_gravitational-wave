92b01cb966a487f6f6349147c1bc0ac94ddc955e
Benda Xu <heroxbd@gentoo.org>
2019-07-19 16:57:14 +0800

    把 matplotlib 的 import 放在前面，防止忘记。

--- fourier.py
+++ fourier.py
@@ -1,3 +1,7 @@
+import matplotlib as mpl
+mpl.use("Agg")
+from matplotlib import pyplot as plt
+
 from plot_raw import load
 import sys, numpy as np
 
@@ -10,10 +14,6 @@ dt = 1.0 / fs
 kH1 = np.abs(np.fft.rfft(strain_H1)) * 2/N
 kL1 = np.abs(np.fft.rfft(strain_L1)) * 2/N
 freq = np.fft.rfftfreq(N, dt)
-
-import matplotlib as mpl
-mpl.use("Agg")
-from matplotlib import pyplot as plt
 
 plt.figure(figsize = (10,7))
 plt.loglog(freq, kH1, label="H1")
